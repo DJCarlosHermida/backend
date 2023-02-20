6ae6f04f26ae209602c29a278e8b0b47b4500e0f
Carlos Hermida <djcarloshermida@outlook.com>
2023-02-20 01:44:53 -0300

    Entregable N 1

--- index.js
+++ index.js
@@ -39,5 +39,5 @@ manager
 manager.addProduct('Parlantes JBL', 'Parlantes JBL de 15"', 60000, 'Imágen No Disponible', 60)
 manager.addProduct('Pantalla Gigante', 'Ideal para proyectar videoClips durante la fiesta', 23000, 'Imágen No Disponible', 2)
 manager.addProduct('Iluminación LED', 'Todo en iluminación para fiestas', 950, 'Imágen No Disponible', 13)
-manager.addProduct('Monitores Studio Rokit', 'Monitores de Studio alto rendimiento', 250, 'Imágen No Disponible', 13)
+manager.addProduct('Monitores Studio Rokit', 'Monitores de Studio alto rendimiento', 250, 'Imágen No Disponible', 23)
 console.log(manager);
