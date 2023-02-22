eb3b51e018ad080e6a12b16c656f034ae8a539f3
DJCarlosHermida <djcarloshermida@outlook.com>
2023-02-22 11:03:03 -0300

    se quita espacios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class ProductManager {
             console.log(`No se pudo cargar los productos: ${error.message}`);
         }
     }
-
+    
     addProduct(title, description, price, thumbnail = 'imágen no disponible', stock = 30) {
         const product = {
             id: this.#generarId(),
@@ -29,7 +29,7 @@ class ProductManager {
     getProductById(id) {
         return this.products.find(this.products.id(1))
     }
-    
+
     #generarId() {
         const id = this.products.length === 0 ? 1 : this.products[this.products.length - 1].id + 1
         return id
@@ -37,7 +37,6 @@ class ProductManager {
 }
 
 const manager = new ProductManager()
-manager
 manager.addProduct('Parlantes JBL', 'Parlantes JBL de 15"', 13000, 'Imágen No Disponible', 60)
 manager.addProduct('Pantalla Gigante', 'Ideal para proyectar videoClips durante la fiesta', 23000, 'Imágen No Disponible', 2)
 manager.addProduct('Iluminación LED', 'Todo en iluminación para fiestas', 950, 'Imágen No Disponible', 13)
