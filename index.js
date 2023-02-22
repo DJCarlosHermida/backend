f05a314939c05115887fdf7c1cc9b82af89d725b
DJCarlosHermida <djcarloshermida@outlook.com>
2023-02-22 10:55:35 -0300

    se quita decimales

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ class ProductManager {
     constructor() {
         this.products = []
     }
+
     getProduct() {
         try {
             console.log(this.products);
-          } catch (error) {
+        } catch (error) {
             console.log(`No se pudo cargar los productos: ${error.message}`);
-          }
+        }
     }
+
     addProduct(title, description, price, thumbnail = 'imágen no disponible', stock = 30) {
-
-
         const product = {
             id: this.#generarId(),
             title: title,
             description: description,
-            price: price / this.#servicios,
+            price: (price / this.#servicios).toFixed(),
             thumbnail,
             stock,
             code: 2023 + this.#generarId()
@@ -25,9 +25,11 @@ class ProductManager {
         this.products.push(product)
 
     }
-    getProductById(id){
+
+    getProductById(id) {
         return this.products.find(this.products.id(1))
     }
+    
     #generarId() {
         const id = this.products.length === 0 ? 1 : this.products[this.products.length - 1].id + 1
         return id
@@ -36,7 +38,7 @@ class ProductManager {
 
 const manager = new ProductManager()
 manager
-manager.addProduct('Parlantes JBL', 'Parlantes JBL de 15"', 60000, 'Imágen No Disponible', 60)
+manager.addProduct('Parlantes JBL', 'Parlantes JBL de 15"', 13000, 'Imágen No Disponible', 60)
 manager.addProduct('Pantalla Gigante', 'Ideal para proyectar videoClips durante la fiesta', 23000, 'Imágen No Disponible', 2)
 manager.addProduct('Iluminación LED', 'Todo en iluminación para fiestas', 950, 'Imágen No Disponible', 13)
 manager.addProduct('Monitores Studio Rokit', 'Monitores de Studio alto rendimiento', 250, 'Imágen No Disponible', 23)
