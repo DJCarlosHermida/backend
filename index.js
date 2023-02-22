48112c4de0194978bcd9674d1779199cee920e63
DJCarlosHermida <djcarloshermida@outlook.com>
2023-02-22 15:14:29 -0300

    Actualización

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class ProductManager {
     #servicios = 0.65
     constructor() {
         this.products = [],
-        this.id = 0
+            this.id = 0
     }
 
     getProducts() {
@@ -10,10 +10,10 @@ class ProductManager {
         //     throw new Error('No Hay Productos')
         // }
         return this.products
-        
+
     }
-    
-    addProduct(title, description, price, thumbnail = 'imágen no disponible', stock = 30){
+
+    addProduct(title, description, price, thumbnail = 'imágen no disponible', stock = 30) {
         const product = {
             id: this.#generarId(),
             title: title,
@@ -23,25 +23,25 @@ class ProductManager {
             stock,
             code: 2023 + this.#generarId()
         }
-        if (!product.price || !product.title || !product.description || !product.thumbnail || !product.stock || !product.code){
+        if (!product.price || !product.title || !product.description || !product.thumbnail || !product.stock || !product.code) {
             console.log('Error: Campos Obligatorios')
-                return
+            return
         }
         if (this.products.some((prod) => prod.code === product.code)) {
             console.log('Error: El Producto Ya Existe');
         }
         this.id++
-        const newProduct = {id: this.id, ...product}
+        const newProduct = { id: this.id, ...product }
         this.products.push(newProduct)
     }
 
     getProductById(id) {
         const product = this.products.find((prod) => prod.id === id)
-            if (product) {
-                return product
-            } else {
-                console.log('Error: No se encontró producto!');
-            }
+        if (product) {
+            return product
+        } else {
+            console.log('Error: No se encontró producto!');
+        }
     }
 
     #generarId() {
@@ -55,9 +55,9 @@ manager.addProduct('Parlantes JBL', 'Parlantes JBL de 15"', 13000, 'Imágen No D
 manager.addProduct('Pantalla Gigante', 'Ideal para proyectar videoClips durante la fiesta', 23000, 'Imágen No Disponible', 2)
 manager.addProduct('Iluminación LED', 'Todo en iluminación para fiestas', 950, 'Imágen No Disponible', 13)
 manager.addProduct('Monitores Studio Rokit', 'Monitores de Studio alto rendimiento', 250, 'Imágen No Disponible', 23)
-console.log('Productos: ',manager.getProducts());
-console.log('Producto Id 1: ',manager.getProductById(1));
-console.log('Producto Id 2: ',manager.getProductById(2));
-console.log('Producto Id 3: ',manager.getProductById(3));
-console.log('Producto Id 4: ',manager.getProductById(4));
-console.log(manager.getProductById(5));
+console.log('Productos: ', manager.getProducts());
+console.log('Producto Id 1: ', manager.getProductById(1));
+console.log('Producto Id 2: ', manager.getProductById(2));
+console.log('Producto Id 3: ', manager.getProductById(3));
+console.log('Producto Id 4: ', manager.getProductById(4));
+console.log(manager.getProductById(50));
