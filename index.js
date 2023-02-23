37f85a12b4d89389c804d3aa991487da95993828
DJCarlosHermida <djcarloshermida@outlook.com>
2023-02-23 10:42:58 -0300

    actualización detalles

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ class ProductManager {
     }
 
     getProducts() {
-        // if (this.products.length == 0) {
-        //     throw new Error('No Hay Productos')
-        // }
+        if (this.products.length == 0) {
+            throw new Error('No Hay Productos')
+        }
         return this.products
 
     }
@@ -23,12 +23,13 @@ class ProductManager {
             stock,
             code: 2023 + this.#generarId()
         }
+        /* CAMBIAR */
         if (!product.price || !product.title || !product.description || !product.thumbnail || !product.stock || !product.code) {
-            console.log('Error: Campos Obligatorios')
-            return
+            return (console.log('Error: Campos Obligatorios'))
         }
+        /* CAMBIAR */
         if (this.products.some((prod) => prod.code === product.code)) {
-            console.log('Error: El Producto Ya Existe');
+            return (console.log('Error: El Producto Ya Existe'))
         }
         this.id++
         const newProduct = { id: this.id, ...product }
@@ -36,12 +37,11 @@ class ProductManager {
     }
 
     getProductById(id) {
+        /* CAMBIAR */
         const product = this.products.find((prod) => prod.id === id)
         if (product) {
-            return product
-        } else {
-            console.log('Error: No se encontró producto!');
-        }
+            return (product)
+        } else {console.log('Error: No se encontró producto!')}
     }
 
     #generarId() {
@@ -55,7 +55,8 @@ manager.addProduct('Parlantes JBL', 'Parlantes JBL de 15"', 13000, 'Imágen No D
 manager.addProduct('Pantalla Gigante', 'Ideal para proyectar videoClips durante la fiesta', 23000, 'Imágen No Disponible', 2)
 manager.addProduct('Iluminación LED', 'Todo en iluminación para fiestas', 950, 'Imágen No Disponible', 13)
 manager.addProduct('Monitores Studio Rokit', 'Monitores de Studio alto rendimiento', 250, 'Imágen No Disponible', 23)
-console.log('Productos: ', manager.getProducts());
+
+console.log('Productos:', manager.getProducts());
 console.log('Producto Id 1: ', manager.getProductById(1));
 console.log('Producto Id 2: ', manager.getProductById(2));
 console.log('Producto Id 3: ', manager.getProductById(3));
