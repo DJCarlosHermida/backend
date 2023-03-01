27c60a98bf131203f359a99cd7cb1e1db803976c
Carlos Hermida <djcarloshermida@outlook.com>
2023-03-01 06:17:21 -0300

    Entregable N2

---
+++ entregable2.js
@@ -0,0 +1,37 @@
+// function saludar() {
+//     return 'Hola DJ'
+// }
+
+// const segundaFun = (a, b) => {
+//     return (a + b)
+// }
+
+// function terceraFun(a) {
+//     return function(){
+
+//     }
+// }
+
+// segundaFun(5, 5)
+// saludar()
+// terceraFun(5)
+
+// const array = ['gato', 'perro', 'ratón', 'gallina']
+//     array.forEach()
+//     array.filter()
+
+// function cbFun(param){
+//     return `el usuario tocó el ${param}`
+// }
+
+// function funDos (p1, callback) {
+//     const respuesta = callback(p1)
+//     return respuesta
+// }
+
+// console.log(funDos('nepe', cbFun));
+
+const n = 50
+
+const resultado = ((n * (n + 1))  / 2)
+console.log(resultado)
