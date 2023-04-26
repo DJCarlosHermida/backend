e7f89220c142d3c5732a20b68fe31dd7950633c7
Carlos Hermida <djcarloshermida@outlook.com>
2023-04-25 21:22:34 -0300

    Entregable Express

--- app/app.js
+++ app/app.js
@@ -3,10 +3,6 @@ import ProductManager from "./ProductManager.js";
 
 const app = express()
 const productManager = new ProductManager('./products.json')
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
 app.use(express.json())
 app.use(express.urlencoded( {extended: true}) )
 
@@ -16,17 +12,17 @@ app.get('/products', async (req, res) => {
     res.json({ products })
 })
 
-app.post('./products', async(req, res) => {
+app.post('/products', async(req, res) => {
     const obj = req.body
     console.log('información', obj);
     const newProduct = await productManager.createProduct(obj)
     res.json({message: 'Producto Created', prodct: newProduct })
 })
 
-app.put('/products/:pib', async (req, res) => {
-    const { idProd } = req.params
+app.put('/products/:pid', async (req, res) => {
+    const { idProd: pid } = req.params
     const obj = req.body
-    const product = await productManager.updateProduct(+idProd, obj)
+    const product = await productManager.updateProduct(+pid, obj)
     res.json({ product })
   })
   
@@ -35,9 +31,9 @@ app.put('/products/:pib', async (req, res) => {
     res.json({ message })
   })
   
-  app.delete('/products/:pib', async (req, res) => {
-    const { idProd } = req.params
-    const message = await productManager.deleteProductById(+idProd)
+  app.delete('/products/:pid', async (req, res) => {
+    const { idProd: pid } = req.params
+    const message = await productManager.deleteProductById(+pid)
     res.json({ message })
   })
 
