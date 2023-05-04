ed4c265412c9b74c4638ef900407da8f5a80b88b
DJCarlosHermida <djcarloshermida@outlook.com>
2023-05-04 09:30:47 -0300

    Actualización

--- app/serverExpress.js
+++ app/serverExpress.js
@@ -6,21 +6,21 @@ app.get('/', (req, res) => {
     console.log(req.query);
     const { limit, order } = req.query
     console.log(limit, order);
-    res.send ('Welcome from Express')
+    res.send('Welcome from Express')
 })
 
 app.get('/products/', (req, res) => {
     console.log(req.params);
-    res.send ('List of Products')
+    res.send('List of Products')
 })
 
 app.get('/products/:pid', (req, res) => {
-    res.send ('Product By ID')
+    res.send('Product By ID')
+})
 app.get('/products/:idProd', (req, res) => {
-    res.send ('Products By ID')
+    res.send('Products By ID')
 })
 
-app.listen(8080, ()=> {
+app.listen(8080, () => {
     console.log('Listening to 8080 port from Express')
 })
-
