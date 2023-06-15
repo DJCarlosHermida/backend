cc4c36d6db1b91d50fb2c83e54196070c16afb68
DJCarlosHermida <djcarloshermida@outlook.com>
2023-06-15 15:25:45 -0300

    Motores de plantillas Views

--- clase09/src/app.js
+++ clase09/src/app.js
@@ -8,16 +8,23 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
+//config
 app.engine('handlebars', handlbars.engine())
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-    res.send('HANDLEBARS')
+    res.send('Bienvenidos')
 })
 
+app.get('/primera', (req, res) => {
+    res.render('primera')
+})
 
+app.get('/segunda', (req, res) => {
+    res.render('segunda')
+})
 
 const PORT = 8080
 app.listen(PORT, () => {
