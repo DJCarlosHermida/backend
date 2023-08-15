76ffa12c6e121e61c5ed933ce72628298c0471ff
Dj Carlos Hermida <djcarloshermida@outlook.com>
2023-08-15 06:33:32 -0300

    .env

--- clase25/src/commander.js
+++ clase25/src/commander.js
@@ -1,10 +1,11 @@
 import { program } from 'commander'
 
 program
-.option('-m, --mode', 'Ambient', 'Development')
+.option('-m, --mode <mode>', 'Ambient', 'Development')
 .option('-d, --debug', 'Variable Modo Debug', false)
 .option('-p, --port', 'PORT of server', 3000)
 .parse(process.argv)
 
 console.log(process.argv);
 console.log(program.opts());
+//console.log(program.args);
--- clase25/src/index.js
+++ clase25/src/index.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
 // console.log('Primer Archivo');
 // console.log('cwd', process.cwd());
-console.log('id', process.argv.slice(2));
+
+dotenv.config()
+
+console.log(process.env.PORT);
